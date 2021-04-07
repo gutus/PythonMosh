47a6b3eb2ced8c09bd09e8a4fb16a851abfd20e3
gutus <gutusnirwanto@gmail.com>
2021-04-07 16:13:08 +0700

    commit on 'main'

---
+++ 10_cmparisor_operatrs.py
@@ -0,0 +1,11 @@
+# COMPARISON OPERATORS
+x = int(input("Masukkan nilai x: "))
+y = int(input("Masukkan nilai y: "))
+
+lebih_besar = x > y
+lebih_kecil = x < y
+sama_persis = x == y
+
+print(f"1. Apakah nilai x={x} lebih besar dari y={y}? {lebih_besar}")
+print(f"2. Apakah nilai x={x} lebih kecil dari y={y}? {lebih_kecil}")
+print(f"3. Apakah nilai x={x} sama persis dengan nilai y={y}? {sama_persis}")
