aadcfdc02faddc60f2f74aaa08bb624d03e7b3ee
gutus <gutusnirwanto@gmail.com>
2021-04-07 09:51:49 +0700

    commit

--- 9_type_conversion.py
+++ 9_type_conversion.py
@@ -1,5 +1,9 @@
-x = input("x: ")
+x = int(input("Nilai x yang kamu pilih: "))
 y = 6
 
 #print(f"Nilai x yang kamu pili:{x} ")
-print(f"Nilai y sudah ada: {y}")
+print(f"Nilai y yang sudah ada: {y}")
+print(f"1. Jika kedua bilangan ditambahkan, maka {x} + {y} = {x+y}")
+print("***" * 3)
+print(
+    f"2. Jika nilai x dikonversi menjadi string dengan str(x), \n maka nilai x menjadi  {str(x)} dan nilai {y} sehingga ketika digabungkan (concatenate) menjadi {str(x)+str(y)}")
