71142b21ad96daf84990ddafddcdac822a1c6606
gutus <gutusnirwanto@gmail.com>
2021-04-07 10:44:53 +0700

    commit on 'main'

--- 9_type_conversion.py
+++ 9_type_conversion.py
@@ -7,3 +7,9 @@ print(f"1. Jika kedua bilangan ditambahkan, maka {x} + {y} = {x+y}")
 print("***" * 3)
 print(
     f"2. Jika nilai x dikonversi menjadi string dengan str(x), \n maka nilai x menjadi  {str(x)} dan nilai {y} sehingga ketika digabungkan (concatenate) menjadi {str(x)+str(y)}")
+# Skenario Memaksa user untuk input hanya bilangan
+a = input("Masukkan nilai a: ")
+if a.isdigit():  # memeriksa apakah nilai yg diinput adalah angka
+    print(f"Nilai a + y adalah {a} + {y} = {a+Y}")
+else:
+    print(f"Masukkan nilai a berupa angka!!!")
