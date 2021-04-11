d2353abf6a8e93a4f186f8d49c78bfd897763f77
gutus <gutusnirwanto@gmail.com>
2021-04-12 00:43:48 +0700

    master

---
+++ test.py
@@ -0,0 +1,8 @@
+hi_income = True
+good_credit= True
+student = False
+
+if hi_income and good_credit and not student:
+    print("Anda berhak mengajukan kredit")
+else:
+    print("Maaf, anda belum berhak mengajukan kredit:")
