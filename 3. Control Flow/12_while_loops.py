29f85f4f8db958f56749d275058e1636a4b19df0
gutus <gutusnirwanto@gmail.com>
2021-04-16 16:07:49 +0700

    master

--- 3. Control Flow/12_while_loops.py
+++ 3. Control Flow/12_while_loops.py
@@ -12,3 +12,13 @@ print(
 while number2 > 0:
     print(number2)
     number2 //= 2  # Menggunakan augmented assignment hasilnya sama dengan number = number // 2
+
+print("Sample ala CLI Command, ketik quit untuk keluar dr program")
+# Sample 3 Command dengan while
+# Simulasi program CLI yang akan quit ketika peintah quit dimasukkan,
+# selebihnya akan ECHO command yg dimasukkan
+command = ""  # Sebagai container menggunakan string kosong
+while command.lower() != "quit":  # menggunakan fungsi lower untuk memastikan semua command yg masuk dievaluasi sebagai lowercase
+    command = input(">>>")  # Prompt yg ditampilkan >>>
+    # Selama belum terdetek quit, input pada variabel command akan dikeluarkan.
+    print("ECHO ", command)
