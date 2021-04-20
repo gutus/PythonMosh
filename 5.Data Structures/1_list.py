a93d810baae59d31096f0f46ca9f2e0430881891
gutus <gutusnirwanto@gmail.com>
2021-04-20 11:29:38 +0700

    master

---
+++ 5.Data Structures/1_list.py
@@ -0,0 +1,15 @@
+# LIST
+letters = ["a", "b", "c"]  # List dari huruf
+matrix = [[0, 1], [2, 3]]
+zeros = [0] * 5
+combined = zeros + letters
+numbers = list(range(20))
+chars = list("Hello World")
+print(letters)
+print(matrix)
+print(zeros)
+print(combined)
+print(numbers)
+print(chars)
+# Untuk menampilkan jumlah (length) karakter penyusun variabel chars
+print(len(chars))
