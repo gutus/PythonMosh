5b14daa7ae65460a4447ed283f7b6f6443ca6bf2
gutus <gutusnirwanto@gmail.com>
2021-04-24 22:43:54 +0700

    master

--- 5.Data Structures/11_list_comprehensions.py
+++ 5.Data Structures/11_list_comprehensions.py
@@ -11,6 +11,8 @@ items = [
 print("Merubah filter object menjadi list dengan filter harga >= 10")
 
 prices = list(map(lambda item: item[1], items))
-prices = [items[1] for otem in items]
+prices = [items[1] for item in items]
 print(prices)
 filtered = list(filter(lambda item: item[1] >= 10, items))
+filtered= [item for item in items if item[1] >= 10]
+print(filtered)
