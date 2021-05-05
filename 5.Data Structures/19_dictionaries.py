2445378367138b343bc2226fa644a7de9d953751
gutus <gutusnirwanto@gmail.com>
2021-05-05 16:48:18 +0700

    master

---
+++ 5.Data Structures/19_dictionaries.py
@@ -0,0 +1,6 @@
+# DICTIONARIES
+# Kumpulan pasangan anatara key:value
+point = {"x": 1, "y": 2}
+print(f"Nilai point awalnya {point}")
+point = dict(x=3, y=5)
+print(f"Nilai point saat ini menjadi \n{point}")
