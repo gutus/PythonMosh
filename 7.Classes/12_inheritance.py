6a7850e615bb0486002580e08b30cd1941a5bec3
gutus <gutusnirwanto@gmail.com>
2021-07-11 20:38:07 +0700

    master

--- 7.Classes/12_inheritance.py
+++ 7.Classes/12_inheritance.py
@@ -1,8 +1,25 @@
 # INHERITANCE
+class Animal:
+    def __init__(self):
+        self.age = 1
 
-class Mammal:
     def eat(self):
         print("eat")
 
+# Animal: Parent, Base
+# Mammal, Fish = Child, Sub, Inherit value from Mamama
+
+
+class Mammal(Animal):
     def walks(self):
         print("walk")
+
+
+class Fish(Animal):
+    def wswim(self):
+        print("swim")
+
+
+sapi = Mammal()
+sapi.eat()
+print(sapi.age)
