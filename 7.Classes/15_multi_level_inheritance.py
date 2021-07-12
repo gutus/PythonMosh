6b76b8e9cba90b2ea26608a680e0895e320db080
gutus <gutusnirwanto@gmail.com>
2021-07-12 20:43:22 +0700

    master

---
+++ 7.Classes/15_multi_level_inheritance.py
@@ -0,0 +1,14 @@
+# Multi Level Inheritance
+
+class Animal:
+    def eat(self):
+        print("Eat")
+
+
+class Bird(Animal):
+    def fly(self):
+        print("Fly")
+
+
+class Chicken(Bird):
+    pass
