3876afdd5466986dbf74db9454cca567a25b5041
gutus <gutusnirwanto@gmail.com>
2021-07-16 07:00:43 +0700

    master

---
+++ 8.Modules/01_creating_modules.py
@@ -0,0 +1,7 @@
+# Membuat Module
+# Module dummy yg dibuat ada pada file sales.py dan cukup diimpor dengan menulis nama filenya saja
+# gunakan fungsi seperlunya, gunakan koma sebagai pemisaj
+from sales import calc_shipping, calc_tax
+
+print(calc_tax())
+print(calc_shipping())
---
+++ 8.Modules/sales.py
@@ -0,0 +1,6 @@
+def calc_shipping():
+    return "Ini contoh fungsi kalkulasi shipping"
+
+
+def calc_tax():
+    return "Ini contoh fungsi kalkulasi pajak"
