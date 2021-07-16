8618d7aa418a5a83f347df8d3acc4e328dd2c7d4
gutus <gutusnirwanto@gmail.com>
2021-07-16 20:09:45 +0700

    master

--- 4.Functions/03_types_of_functions.py
+++ 4.Functions/03_types_of_functions.py
@@ -7,15 +7,19 @@ def greet(name):
 greet("Gutus")
 
 print("Tipe fungsi lainnya dengan menghasilkan output berupa variabel")
-#Contoh di bawah ini sampel print biasa dan print ke dalam file content.txt, canggih bener.
+# Contoh di bawah ini sampel print biasa dan print ke dalam file content.txt, canggih bener.
+
 
 def salam(name):
     return f"Hi {name}"
 
 
-message = salam("Gutus Nirwanto yg ganteng tajir melintir level global") #Tulis argumen di sini, dan cek hasil penulisan di file content.txt
+# Tulis argumen di sini, dan cek hasil penulisan di file content.txt
+message = salam("Gutus Nirwanto yg ganteng tajir melintir level global")
 
-print(message) #Perintah sederhana print di terminal
-tulisan = open("4.Function_content.txt", "w") #Perintah membuka file content.txt dengan type writeable (menulis file, tidak hanya membacanya saja) dan di assign ke varibel tulisan.
-#Fungsi open file writeable yg disembunyikan ke dalam variabel tulisan.  Sehingga di kode selanjutnya cukup memanggil variabel tulisan saja untuk fungsi penulisan.  Panjang bener oenjabarannya :)
-tulisan.write(message) #Memerintahkan penulisan variabel message terhadap variabel tulisan pada file sebelumnya
+print(message)  # Perintah sederhana print di terminal
+# Perintah membuka file content.txt dengan type writeable (menulis file, tidak hanya membacanya saja) dan di assign ke varibel tulisan.
+tulisan = open("4.Function_type_content.txt", "w")
+# Fungsi open file writeable yg disembunyikan ke dalam variabel tulisan.  Sehingga di kode selanjutnya cukup memanggil variabel tulisan saja untuk fungsi penulisan.  Panjang bener oenjabarannya :)
+# Memerintahkan penulisan variabel message terhadap variabel tulisan pada file sebelumnya
+tulisan.write(message)
